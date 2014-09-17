d8a24906bdbb0d3bcbadbdd1560e20057e169606
Erik Nyhus <eriknyh@stud.ntnu.no>
2014-09-17 18:42:59 +0200

    Add hackish reset task to db_admin.

--- db_admin/src/main/java/no/flaming_adventure/db_admin/DBAdmin.java
+++ db_admin/src/main/java/no/flaming_adventure/db_admin/DBAdmin.java
@@ -1,26 +1,46 @@
 package no.flaming_adventure.db_admin;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
 
 import org.apache.commons.cli.CommandLine;
+import org.apache.commons.io.IOUtils;
 
 public class DBAdmin {
     public static void main(String[] args) {
-        /* With a lack of something better to do, lets test if sqlite works. */
-        Connection conn = null;
         try {
-            CommandLine line = Cli.parse(args);
-
             Class.forName("org.sqlite.JDBC");
-            conn = DriverManager.getConnection("jdbc:sqlite::memory:");
-            Statement stmt = conn.createStatement();
-            String sql = "CREATE TABLE t1 (ID INT); CREATE TABLE t2 (ID INT);";
-            stmt.execute(sql);
-            stmt.close();
+            CommandLine line = Cli.parse(args);
+            if (line.hasOption("reset")) {
+                DBAdmin.reset("./test.db");
+            }
         } catch (Exception e) {
             System.err.println(e);
             System.exit(1);
         }
-        System.out.println("Tables created successfully.");
+    }
+
+    /**
+     * Reset the database at the given path.
+     *
+     * @param db_path path to the database file to reset.
+     * @throws SQLException
+     */
+    public static void reset(String db_path) throws SQLException, IOException {
+        InputStream stream = DBAdmin.class.getClassLoader().getResourceAsStream("db_reset.sql");
+        /* FIXME: Hack to allow execution of all statements in the file. */
+        String[] sql = IOUtils.toString(stream).split(";");
+
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:" + db_path);
+        Statement statement = connection.createStatement();
+
+        for (String s: sql) {
+            /* FIXME: Hack to avoid executing whitespace. */
+            if (s == sql[sql.length -1]) { break; }
+            statement.addBatch(s);
+        }
+        statement.executeBatch();
+        statement.close();
     }
 }
