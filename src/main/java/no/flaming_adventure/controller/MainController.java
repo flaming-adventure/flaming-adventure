3a0d2ae6decb23a6df5556ba533baa6e26af03b0
Erik Nyhus <eriknyh@stud.ntnu.no>
2014-11-11 16:25:46 +0100

    Reload tab data every time a tab is selected.

--- src/main/java/no/flaming_adventure/controller/MainController.java
+++ src/main/java/no/flaming_adventure/controller/MainController.java
@@ -1,6 +1,8 @@
 package no.flaming_adventure.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import no.flaming_adventure.model.DataModel;
 
 import java.util.function.Consumer;
@@ -26,22 +28,42 @@ public class MainController {
     @FXML private EquipmentTableController      equipmentTableController;
     @FXML private BrokenItemTableController     brokenItemTableController;
 
+    @FXML private TabPane   tabPane;
+    @FXML private Tab       reservationFormTab;
+    @FXML private Tab       reservationTableTab;
+    @FXML private Tab       forgottenTab;
+    @FXML private Tab       equipmentTab;
+    @FXML private Tab       brokenTab;
+
     public MainController(DataModel dataModel, Consumer<Throwable> unhandledExceptionHook) {
         this.dataModel  = dataModel;
         this.unhandledExceptionHook = unhandledExceptionHook;
     }
 
-    @FXML protected void initialize() {
+    @FXML private void initialize() {
         reservationFormController.inject(dataModel, unhandledExceptionHook);
         reservationTableController.inject(dataModel, unhandledExceptionHook);
         forgottenTableController.inject(dataModel, unhandledExceptionHook);
         brokenItemTableController.inject(dataModel, unhandledExceptionHook);
         equipmentTableController.inject(dataModel, unhandledExceptionHook);
 
-        reservationFormController.load();
-        reservationTableController.load();
-        forgottenTableController.load();
-        brokenItemTableController.load();
-        equipmentTableController.load();
+        tabPane.getSelectionModel().selectedItemProperty()
+                .addListener((observable, oldValue, tab) -> loadTab(tab));
+
+        loadTab(tabPane.getSelectionModel().getSelectedItem());
+    }
+
+    private void loadTab(Tab tab) {
+        if (tab == reservationFormTab) {
+            reservationFormController.load();
+        } else if (tab == reservationTableTab) {
+            reservationTableController.load();
+        } else if (tab == forgottenTab) {
+            forgottenTableController.load();
+        } else if (tab == equipmentTab) {
+            equipmentTableController.load();
+        } else if (tab == brokenTab) {
+            brokenItemTableController.load();
+        }
     }
 }
