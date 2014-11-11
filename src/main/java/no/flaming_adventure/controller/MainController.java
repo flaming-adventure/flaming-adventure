c68b2ccdcf6025403d3424c36ed851574f4f9dc3
Erik Nyhus <eriknyh@stud.ntnu.no>
2014-11-11 22:56:43 +0100

    Remove TODOs.

--- src/main/java/no/flaming_adventure/controller/MainController.java
+++ src/main/java/no/flaming_adventure/controller/MainController.java
@@ -11,11 +11,7 @@ import java.util.function.Consumer;
  * Controller for the main view.
  *
  * <ul>
- *     <li>TODO (enhancement): unify date handling application-wide.
- *     <li>TODO (enhancement): only initialize controllers when the corresponding tab is first opened.
  *     <li>TODO #38 (enhancement): add data validation application-wide.
- *     <li>TODO #43 (enhancement): add error interface.
- *     <li>TODO (bug): handle empty selections.
  * </ul>
  */
 public class MainController {
