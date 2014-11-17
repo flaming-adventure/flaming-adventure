40530d8b267f38defc82d341ef66352da82da740
Erik Nyhus <eriknyh@stud.ntnu.no>
2014-11-17 11:03:00 +0100

    Documentation.

--- src/main/java/no/flaming_adventure/util/ListUpdateListener.java
+++ src/main/java/no/flaming_adventure/util/ListUpdateListener.java
@@ -4,13 +4,50 @@ import javafx.collections.ListChangeListener;
 
 import java.util.function.Consumer;
 
+/**
+ * A {@link javafx.collections.ListChangeListener list change listener} that calls a
+ * given function for every updated item every time the list is updated.
+ *
+ * @param <T> the type of the list elements.
+ */
 public class ListUpdateListener<T> implements ListChangeListener<T> {
+
+    /***************************************************************************
+     *                                                                         *
+     * Instance Variables                                                      *
+     *                                                                         *
+     **************************************************************************/
+
     private final Consumer<T> updateHook;
 
+    /***************************************************************************
+     *                                                                         *
+     * Constructors                                                            *
+     *                                                                         *
+     **************************************************************************/
+
+    /**
+     * Construct a list update listener with the given update function.
+     *
+     * @param updateHook a function taking a single object of type {@link T}. This
+     *                   function will be called for every updated item every time
+     *                   the list is updated.
+     */
     public ListUpdateListener(Consumer<T> updateHook) {
         this.updateHook = updateHook;
     }
 
+    /***************************************************************************
+     *                                                                         *
+     * Public API                                                              *
+     *                                                                         *
+     **************************************************************************/
+
+    /**
+     * Function to be called whenever the list is modified in any way.
+     *
+     * @param change a set of changes.
+     */
     @Override public void onChanged(Change<? extends T> change) {
         while (change.next()) {
             if (change.wasUpdated()) {
